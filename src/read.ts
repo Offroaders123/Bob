f33587f5e1e880d269df97a07ad90b5cd6f61ed3
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-11-10 02:43:41 -0800

    Further Read Setup
    Writing new APIs in TS is awesome, definitely would recommend.

--- src/read.ts
+++ src/read.ts
@@ -2,6 +2,43 @@ import { getTag, TAG } from "./primitive.js";
 
 import type { BOBArray, BOBObject, BOBPrimitive } from "./primitive.js";
 
-export function read(data: Uint8Array): BOBPrimitive {}
+export function read(data: Uint8Array): BOBPrimitive {
+  const reader: BOBReader = new BOBReader(data);
+  return reader.primitive();
+}
 
-export class BOBReader {}
+export class BOBReader {
+  #byteOffset: number = 0;
+  #data: Uint8Array;
+  #view: DataView;
+  readonly #decoder: TextDecoder = new TextDecoder();
+
+  constructor(data: Uint8Array) {
+    this.#data = data;
+    this.#view = new DataView(this.#data.buffer, this.#data.byteOffset, this.#data.byteLength);
+  }
+
+  #allocate(byteLength: number): void {
+    if (this.#byteOffset + byteLength > this.#data.byteLength) {
+      throw new Error("Ran out of bytes to read, unexpectedly reached the end of the buffer");
+    }
+  }
+
+  #tag(): TAG {}
+
+  #byte(): number {}
+
+  primitive(): BOBPrimitive {}
+
+  string(): string {}
+
+  number(): number {}
+
+  boolean(): boolean {}
+
+  null(): null {}
+
+  array(): BOBArray<BOBPrimitive | undefined> {}
+
+  object(): BOBObject {}
+}
--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,15 @@
 import { writeFile } from "node:fs/promises";
-import { write } from "../src/index.js";
+import { read, write } from "../src/index.js";
 import data from "../package.json" with { type: "json" };
+
+import type { BOBPrimitive } from "../src/index.js";
 
 console.log(data);
 
 const binary: Uint8Array = write(data);
-console.log(binary);
+console.log(Buffer.from(binary.buffer));
+
+const redata: BOBPrimitive = read(binary);
+console.log(redata);
 
 await writeFile(new URL("./package.bob", import.meta.url), binary);
