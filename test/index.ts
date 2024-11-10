0b90b76efb2069323f1b5c505ded57bc3158c6ff
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-11-09 20:36:42 -0800

    Binary Itself!

--- test/index.ts
+++ test/index.ts
@@ -1,3 +1,4 @@
+import { writeFile } from "node:fs/promises";
 import { write } from "../src/write.js";
 import data from "../package.json" with { type: "json" };
 
@@ -5,3 +6,5 @@ console.log(data);
 
 const binary: Uint8Array = write(data);
 console.log(binary);
+
+await writeFile(new URL("./package.bob", import.meta.url), binary);
