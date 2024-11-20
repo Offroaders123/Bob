1e25aa1e1da3524069ad9b75e8b1a00e748a5b97
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-11-20 12:14:23 -0800

    Read-Write Test
    This ensures that having written and reading the data, it will be the exact same shape both ways.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,21 @@
-import { writeFile } from "node:fs/promises";
+import { describe, it } from "node:test";
 import { deepStrictEqual } from "node:assert";
+import { writeFile } from "node:fs/promises";
 import { read, write } from "../src/index.js";
 import data from "../package.json" with { type: "json" };
 
-console.log(data);
+describe("Read-Write", () => {
+  it("package.json", () => {
+    // console.log(data);
 
-const binary: Uint8Array = write(data);
-console.log(Buffer.from(binary.buffer));
+    const binary: Uint8Array = write(data);
+    // console.log(Buffer.from(binary.buffer));
 
-await writeFile(new URL("./package.bob", import.meta.url), binary);
+    writeFile(new URL("./package.bob", import.meta.url), binary);
 
-const redata: typeof data = read(binary);
-console.log(redata);
+    const redata: typeof data = read(binary);
+    // console.log(redata);
 
-deepStrictEqual(data, redata);
+    deepStrictEqual(data, redata);
+  });
+});
