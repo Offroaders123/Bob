b19bd7c560ef28a0593f3939bb81a63d03085a6b
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-11-20 12:42:06 -0800

    All Test Added

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,20 +2,33 @@ import { describe, it } from "node:test";
 import { deepStrictEqual } from "node:assert";
 import { writeFile } from "node:fs/promises";
 import { read, write } from "../src/index.js";
-import data from "../package.json" with { type: "json" };
+
+import type { BOBPrimitive } from "../src/index.js";
+
+import pakcage from "../package.json" with { type: "json" };
+import all from "./all.json" with { type: "json" };
+
+const tests = [
+  ["./package.bob", pakcage, true],
+  ["./all.json", all]
+] as const satisfies [string, BOBPrimitive, true?][];
 
 describe("Read-Write", () => {
-  it("package.json", () => {
-    // console.log(data);
-
-    const binary: Uint8Array = write(data);
-    // console.log(Buffer.from(binary.buffer));
-
-    writeFile(new URL("./package.bob", import.meta.url), binary);
-
-    const redata: typeof data = read(binary);
-    // console.log(redata);
-
-    deepStrictEqual(data, redata);
-  });
+  for (const [name, data, shouldWrite] of tests) {
+    it(name, () => readWriteTest(name, data, shouldWrite));
+  }
 });
+
+function readWriteTest<T extends BOBPrimitive>(name: string, data: T, shouldWrite: boolean = false): void {
+  // console.log(data);
+
+  const binary: Uint8Array = write(data);
+  // console.log(Buffer.from(binary.buffer));
+
+  if (shouldWrite) writeFile(new URL(name, import.meta.url), binary);
+
+  const redata: T = read(binary);
+  // console.log(redata);
+
+  deepStrictEqual(data, redata);
+}
