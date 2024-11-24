307c299d3dbcae6125f574a5777183910c9f48aa
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-11-24 02:20:23 -0800

    Replacer-Reviver Exploration
    Realizing that implementing custom primitives don't really make sense, when you can instead just implement your own custom replacer and hydrater. I'm curious how this can relate to NBTify too, as well as in regards to this project itself. Maybe I can build new formats that are recursive to the base primitives themselves, but implement them by way of building into the base primitives themselves, rather than needing to implement a whole new tag schema into the format too. So it would be like how zip files allow for new metadata to be added, and that extra space in the file format/'syntax' (in terms of TypeScript in relation with JS 👀) can instead be used to define your own types of formats. I really like this concept! It's like dependency injection, but for file formats! I guess it's like how sometimes Minecraft stores JSON inside of NBT string tags.
    
    https://github.com/Offroaders123/NBTify/issues/33
    
    Lightwork is absolutely one of my favorite albums, ever. The whole concept behind it has unapologetically changed me as a person.

---
+++ src/JSON-replacer-reviver.ts
@@ -0,0 +1,62 @@
+// https://www.youtube.com/watch?v=wEtyYNFOIew&list=PL2B1pJtmgd8B6MlOddpULaLbTdEncOWj4
+// https://www.google.com/search?q=json+replacer+reviver
+// https://gist.github.com/jimmywarting/a6ae45a9f445ca352ed62374a2855ff2
+// https://stackoverflow.com/questions/65876907/how-to-add-a-global-replacer-for-json-stringify-and-global-reviver-for-json-pars
+// https://github.com/jprichardson/buffer-json
+// https://dev.to/dvddpl/revive-your-json-examples-with-tdd-and-composition-3
+
+import { deepStrictEqual } from "node:assert";
+
+// Now I'm listening to Devy Lightwork, and relaxing asmr :)
+
+const heya = {
+  nice: new Uint8Array([45, 82, 19, 43, 0, 1, 2, 3, 4, 5]),
+  heya: 25n,
+  what: [
+    {
+      aa: "Sweet",
+      l: {}
+    }
+  ],
+  sets: new Set([
+    new Set([25, {}]),
+    new Set([92, 5n])
+  ])
+};
+console.log(heya);
+
+export type Replacer = (this: any, key: string, value: any) => any;
+
+export type Reviver = (this: any, key: string, value: any) => any;
+
+const replacer: Replacer = function(_key, value) {
+  // console.log("THIS", _key, this);
+  // console.log("VALUE", value);
+  switch (true) {
+    case value instanceof Uint8Array: return ["$__Uint8Array", ...value];
+    case typeof value === "bigint": return ["$__bigint", value.toString()];
+    case value instanceof Set: return ["$__Set", ...value];
+    default: return value;
+  }
+  // return value;
+};
+
+const reviver: Reviver = function(_key, value) {
+  // console.log("THIS", _key, this);
+  // console.log("VALUE", value);
+  if (!Array.isArray(value)) return value;
+  switch (value[0]) {
+    case "$__Uint8Array": return Uint8Array.from(value.slice(1));
+    case "$__bigint": return BigInt(value[1]);
+    case "$__Set": return new Set(value.slice(1));
+    default: return value;
+  }
+  // return value;
+};
+
+const stringy: string = JSON.stringify(heya, replacer);
+
+const heyay: typeof heya = JSON.parse(stringy, reviver);
+console.log(heyay);
+
+deepStrictEqual(heya, heyay);
